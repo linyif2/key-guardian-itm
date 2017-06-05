020257ddacb6e8d6a6ac5a9cbbfbb570cc32e854
linyif2 <568833891@qq.com>
2017-06-05 22:08:23 +0800

    角色配置页优化

--- src/page/system/roleCfg.vue
+++ src/page/system/roleCfg.vue
@@ -2,14 +2,16 @@
 	<div>
 		<el-row>
 			<el-col :span="6" class="role-list">
-				<h4>角色列表</h4>
-				<li>超级管理员</li>
-				<li>项目负责人</li>
-				<li>开发人员</li>
-				<li>运维人员</li>
-				<li>游客</li>
+				<h4>角色列表 <el-button type="primary" size="mini" icon="plus"></el-button></h4>
+				<ul>
+					<li data-rid="1" :class="{active:roleActive=='1'}" @click="selectRole($event)">超级管理员</li>
+					<li data-rid="2" :class="{active:roleActive=='2'}" @click="selectRole($event)">项目负责人</li>
+					<li data-rid="3" :class="{active:roleActive=='3'}" @click="selectRole($event)">开发人员</li>
+					<li data-rid="4" :class="{active:roleActive=='4'}" @click="selectRole($event)">运维人员</li>
+					<li data-rid="5" :class="{active:roleActive=='5'}" @click="selectRole($event)">游客</li>
+				</ul>
 			</el-col>
-			<el-col :span="13" class="role-content">
+			<el-col :span="13" class="menu-tree">
 				<h4>菜单树</h4>
 				<el-tree :data="data2" show-checkbox node-key="id" :default-expanded-keys="[2, 3]" :default-checked-keys="[5]" :props="defaultProps">
 				</el-tree>
@@ -37,6 +39,7 @@
 		name: 'roleCfg',
 		data() {
 			return {
+				roleActive: '',
 				checkList: [],
 				data2: [{
 					id: 1,
@@ -78,24 +81,83 @@
 					label: 'label'
 				}
 			};
+		},
+		methods: {
+			selectRole(event) {
+				this.roleActive = event.currentTarget.dataset.rid
+			}
 		}
 	}
 </script>
 
 <style scoped>
-	*{list-style: none;}
-	.role-list li{
+	* {
+		list-style: none;
+	}
+	
+	h4 {
+		padding: 10px;
+		margin: 0;
+		background-color: #eef1f6;
+		color: #48576a;
+		height: 21px;
+		border-bottom: 1px solid #d1dbe5;
+	}
+	h4 button{
+		float: right;
+	}
+	.role-list {
+		border: 1px solid #d1dbe5;
+		border-right: 0;
+	}
+	
+	.role-list ul {
+		height: 500px;
+		overflow-y: auto;
+		padding: 0 10px;
+		margin: 0;
+	}
+	
+	.role-list li {
 		height: 35px;
 		line-height: 35px;
-		padding:0 5px;
+		padding: 0 10px;
 		font-size: 15px;
 	}
-	.role-list li:hover{
-		background-color: #e5e5e5;
+	
+	.role-list li:hover {
+		background-color: #e4e8f1;
 		cursor: pointer;
 	}
+	
+	.role-list li.active {
+		background-color: #20a0ff;
+		border-color: #0190fe;
+		color: #FFFFFF;
+	}
+	
+	.menu-tree {
+		border: 1px solid #d1dbe5;
+		border-right: 0;
+	}
+	
+	.menu-tree .el-tree {
+		height: 500px;
+		border: 0;
+	}
+	
+	.auth-list {
+		border: 1px solid #d1dbe5;
+	}
+	
 	.auth-list h5 {
 		margin: 10px;
+	}
+	
+	.auth-list .el-checkbox-group {
+		padding: 0 10px;
+		height: 500px;
+		overflow-y: auto;
 	}
 	
 	.auth-list .el-checkbox {
